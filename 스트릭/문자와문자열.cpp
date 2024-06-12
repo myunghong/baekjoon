aa0180eeba37bc829a52e7beb372d4925bd28d0b
Seojun <auddml321@hanyang.ac.kr>
2024-06-12 15:18:05 +0900

    2024/06/11 시험기간 스트릭

---
+++ 스트릭/문자와문자열.cpp
@@ -0,0 +1,13 @@
+#include<algorithm>
+#include<iostream>
+#include<string.h>
+using namespace std;
+int main(void){
+    string str;
+    int n;
+    cin >> str >> n;
+    cout << str[n-1];
+
+
+    return 0;
+}
