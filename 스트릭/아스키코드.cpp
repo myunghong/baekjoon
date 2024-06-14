5591283db43408344bc6da4a52bb576d71d2a0ba
Seojun <auddml321@hanyang.ac.kr>
2024-06-14 19:57:02 +0900

    2024/06/14 시험기간 스트릭

---
+++ 스트릭/아스키코드.cpp
@@ -0,0 +1,11 @@
+#include<algorithm>
+#include<iostream>
+#include<string.h>
+using namespace std;
+int main(void){
+    string s;
+    cin >> s;
+    int a = s[0];
+    cout << a;
+    return 0;
+}
