9a7c505ae52bd35880d52d00c4bf9d5013613f21
Seojun <auddml321@hanyang.ac.kr>
2024-06-20 03:07:39 +0900

    2024/06/18 시험기간 스트릭

---
+++ 스트릭/단어의개수.cpp
@@ -0,0 +1,14 @@
+#include<algorithm>
+#include<iostream>
+#include<string.h>
+using namespace std;
+int main(void){
+    string board;
+    int cnt = 0;
+    int i = 0;
+    while(cin >> board){
+        i++;
+    }
+    cout << i;
+    return 0;
+}
