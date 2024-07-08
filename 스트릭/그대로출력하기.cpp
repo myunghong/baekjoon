4eea04f8ebf181c8ef158b26812246c1879e8c6d
Seojun <auddml321@hanyang.ac.kr>
2024-07-08 21:59:43 +0900

    2024/07/08 스트릭

---
+++ 스트릭/그대로출력하기.cpp
@@ -0,0 +1,14 @@
+#include<algorithm>
+#include<iostream>
+#include<vector>
+#include<string>
+using namespace std;
+int main(void){
+    cin.tie(0);
+    ios::sync_with_stdio(0);
+    string str;
+    while(getline(cin, str)){
+        cout << str << '\n';
+    }
+    return 0;
+}
