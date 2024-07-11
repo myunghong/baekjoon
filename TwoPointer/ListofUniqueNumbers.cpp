6a654cb9a6c46d0a79c496d9476c29e28b0157ad
Seojun <auddml321@hanyang.ac.kr>
2024-07-11 22:33:49 +0900

    2024/07/11 투포인터

--- TwoPointer/ListofUniqueNumbers.cpp
+++ TwoPointer/ListofUniqueNumbers.cpp
@@ -1,16 +1,31 @@
 #include<algorithm>
 #include<iostream>
+#include<unordered_map>
 using namespace std;
 #define MAX 100005
 int board[MAX];
+unordered_map<int,int> visit;
 int main(void){
-    int n, en = 0, st = 0; cin >> n;
+    cin.tie(0);
+    ios::sync_with_stdio(0);
+    int n, en = 0,st = 0; cin >> n;
+    long long cnt = 0;
     for(int i = 0;i <n; i++){
         cin >> board[i];
     }
-    while(en < n && st < n){
-        
+    board[n] = 100005;
+    visit[board[n]] = 100;
+    while(st < n){
+        if(visit[board[en]] == 0){
+            visit[board[en]]++;
+            en++;
+        }
+        else{
+            visit[board[st]]--;
+            cnt += en - st;
+            st++;
+        }
     }
-
+    cout << cnt;
     return 0;
 }
