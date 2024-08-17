f6a32e569f8f9a5ed94147cfff0f174bfa6fff0b
Seojun <auddml321@hanyang.ac.kr>
2024-08-18 00:29:28 +0900

    2024/08/18 스트릭 ㅎㅎ

---
+++ 스트릭/알고리즘수행시간.cpp
@@ -0,0 +1,10 @@
+#include<algorithm>
+#include<iostream>
+using namespace std;
+int main(void){
+    cin.tie(0);
+    ios::sync_with_stdio(0);
+    long long n; cin >> n;
+    cout << n*n*n << "\n3";
+    return 0;
+}
---
+++ 스트릭/알고리즘수행시간3.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+int main(void){
+    cin.tie(0);
+    ios::sync_with_stdio(0);
+    long long n; cin >> n;
+    cout << n*n << "\n2";
+    return 0;
+}
