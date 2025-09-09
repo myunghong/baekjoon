2fc8a36e0f850761dce6c54aaa28a03345ba0950
Seojun <auddml321@hanyang.ac.kr>
2025-09-09 16:08:05 +0900

    2025/09/09
    하나의 스택으로 여러 입력에 대해 처리하는 경우 한 입력에 대한 출력이 끝나면 스택을 초기화해줘야한다.

--- 스택/균형잡힌세상.cpp
+++ 스택/균형잡힌세상.cpp
@@ -5,16 +5,33 @@ using namespace std;
 int main(void){
     cin.tie(nullptr);
     ios::sync_with_stdio(false);
-    stack<string> stk;
     string str = "";
     while(1){
+        stack<char> stk;
         getline(cin, str);
         if(str == ".") break;
-        for(int i = 0; i<str.length(), i++){
-            if(str[i] == "[" || str[i] == "("){
+        for(int i = 0; i<str.length(); i++){
+            if(str[i] == '[' || str[i] == '('){
                 stk.push(str[i]);
             }
-            if(str[i] == "]")
+            if(str[i] == ']'){
+                if(!stk.empty() && stk.top() == '[') stk.pop();
+                else {
+                cout << "no\n";
+                break;
+                }
+            }
+            else if(str[i] == ')'){
+                if(!stk.empty() && stk.top() == '(') stk.pop();
+                else {
+                cout << "no\n";
+                break;
+                }
+            }
+            if(i == str.length() - 1) {
+                if(stk.empty()) cout << "yes\n";
+                else cout << "no\n";
+            }
         }
     }
     return 0;
